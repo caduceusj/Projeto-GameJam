eaaa70bdec91aa7bb95d8bf2f7e6f7998f7e6b8f
thommy000 <92895609+thommy000@users.noreply.github.com>
2022-10-16 09:03:32 -0300

    MenuAdded

---
+++ Scripts/Intro.gd
@@ -0,0 +1,23 @@
+extends Node2D
+
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	$AnimationPlayer.play("Intro1.1")
+	yield($AnimationPlayer, "animation_finished")
+	get_tree().change_scene("res://Cenas/Menu.tscn")
+	pass # Replace with function body.
+
+
+func _input(event):
+		if event.is_action_pressed("LMB"):
+			$AnimationPlayer.stop()
+			get_tree().change_scene("res://Cenas/Menu.tscn")
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
