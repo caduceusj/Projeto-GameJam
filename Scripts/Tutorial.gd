8c3f33a3d1f652e1153c61729bdbc6d91d3ce3a5
caduceusj <joaoanisiomn@hotmail.com>
2022-10-16 19:29:04 -0300

    eoq

--- Scripts/Tutorial.gd
+++ Scripts/Tutorial.gd
@@ -2,6 +2,9 @@ extends Node2D
 
 
 func _ready():
+	$AnimationPlayer.play("text")
+	yield($AnimationPlayer, "animation_finished")
+	var camera = get_node("res://Cenas/Player.tscn/Camera2D")
 	pass
 	
 func nextLevel():
@@ -13,3 +16,11 @@ func _on_Area2D_body_entered(_body):
 		nextLevel()
 	else:
 		pass
+
+
+
+
+
+func _on_ArcoFlecha_body_entered(body):
+	if body.is_in_group("player"):
+		$Arco.hide()
