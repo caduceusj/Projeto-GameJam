975b46401089f748a5817d7dd9992fc68f271651
unknown <ping.rafa99@gmail.com>
2022-10-16 09:38:21 -0300

    s

--- Scripts/Intro.gd
+++ Scripts/Intro.gd
@@ -10,14 +10,14 @@ extends Node2D
 func _ready():
 	$AnimationPlayer.play("Intro1.1")
 	yield($AnimationPlayer, "animation_finished")
-	get_tree().change_scene("res://Cenas/Menu.tscn")
+	get_tree().change_scene("res://Cenas/Scene0.tscn")
 	pass # Replace with function body.
 
 
 func _input(event):
 		if event.is_action_pressed("LMB"):
 			$AnimationPlayer.stop()
-			get_tree().change_scene("res://Cenas/Menu.tscn")
+			get_tree().change_scene("res://Cenas/Scene0.tscn")
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
 #	pass
